ff3c454c7e36c4f826dbb2e33e5d1936472ccdc5
skavhaug <skavhaug@56e15811-b3da-0310-bf84-f0ae79e16665>
2005-09-21 13:53:29 +0000

    distutils setup file added

---
+++ trunk/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+"""
+At the moment, this setup.py file is very crude. I expect it will
+only work on Debian systems (no platform independence yet).
+
+Make sure that you have at least SWIG version 1.3.24 and GiNaC
+version 1.2.3.
+
+Ola Skavhaug
+Simula Research Laboratory
+"""
+
+e = Extension(name='_swiginac', 
+              sources=['swiginac.i'],
+              include_dirs=['/usr/include/ginac'],
+              library_dirs=['/usr/lib'],
+              libraries=['ginac'],
+              )
+
+setup(name='swiginac',
+    version='0.1',
+    description='swiginac extention module',
+    author='Ola Skavhaug',
+    author_email='skavhaug@simula.no',
+    url='http://swiginac.berlios.de/',
+    ext_modules=[e],
+    py_modules = ['swiginac'],
+    )
